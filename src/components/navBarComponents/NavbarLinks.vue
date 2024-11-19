37b8327b2b922d183f151eab7fe246427d199233
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-19 22:24:02 +0100

    feat: added NavLinks component and updated AppHeader for dynamic navigation links

---
+++ src/components/navBarComponents/NavbarLinks.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "NavLinks",
+    props: {
+        links: {
+            type: Array,
+            required: true,
+        },
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li v-for="(link, index) in links" :key="index" class="nav-item">
+            <a class="nav-link" href="#">{{ link.name }}</a>
+        </li>
+    </ul>
+</template>
+
+<style scoped></style>
