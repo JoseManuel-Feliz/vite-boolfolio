a918097e4d3f7af971599cfc8d8422c24030873d
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-19 17:35:29 +0100

    feat(components): added and imported into App.vue AppHeader and AppFooter components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>ciaoooooo sono un footer</h2>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>1 2 3 prova sasa</h2>
+</template>
+
+<style scoped></style>
