af239c83ee3151306bb58dec727dc45a52fefcfc
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-19 19:29:03 +0100

    feat: added navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,45 @@ export default {
 </script>
 
 <template>
-    <h2>1 2 3 prova sasa</h2>
+
+    <Header>
+        <div class="container">
+
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <!-- Todo title (or maybe a logo) -->
+                    <a class="navbar-brand" href="#">My Projects</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About me</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contect me</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    </Header>
+
 </template>
 
 <style scoped></style>
