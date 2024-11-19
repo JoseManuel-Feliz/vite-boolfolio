1e94fe20bedfa8f7fe7ce5b613d9ee891b9531b0
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-19 22:24:02 +0100

    feat: added NavLinks component and updated AppHeader for dynamic navigation links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,6 @@
 <script>
 import NavTitle from './navBarComponents/NavbarTitle.vue';
+import NavLinks from './navBarComponents/NavbarLinks.vue';
 export default {
 
     name: "AppHeader",
@@ -7,10 +8,17 @@ export default {
     data() {
         return {
             title: "My Projects",
+            links:
+                [
+                    { "name": "Home" },
+                    { "name": "Projects" },
+                    { "name": "About me" },
+                ],
         };
     },
     components: {
         NavTitle,
+        NavLinks,
     },
 
 };
@@ -31,20 +39,9 @@ export default {
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About me</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Projects</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contect me</a>
-                            </li>
-                        </ul>
+                        <!-- Navbar Links -->
+                        <NavLinks :links="links" />
+
                         <form class="d-flex" role="search">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" type="submit">Search</button>
