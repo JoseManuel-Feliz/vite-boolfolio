90f83e6001aac7a2fbe2de53a30147c658e69636
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 18:01:44 +0100

    feat: imported AppProjects component in routes

--- src/js/router.js
+++ src/js/router.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import AppProjects from '../pages/AppProjects.vue';
+
+const routes = [
+    { path: '/projects', component: AppProjects, name: "projects" },
+]
 
 const router = createRouter({
     history: createWebHistory(),
-
-    routes: []
+    routes,
 });
 export { router };
