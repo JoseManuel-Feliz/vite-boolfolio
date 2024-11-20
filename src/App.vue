357bbeb6b44990bd049f585b606b521feaf0cdd7
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 02:38:00 +0100

    feat: added axios import and configured API endpoint for projects

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
+// Imported AppHeader
 import AppHeader from "./components/AppHeader.vue";
+// Imported AppFooter
 import AppFooter from "./components/AppFooter.vue";
+// Imported Axios
+import axios from 'axios';
 
 export default {
   data() {
     return {
+      apiUri: 'http://127.0.0.1:8000/api/projects',
     }
+  },
+  mounted() {
+    axios.get(this.apiUri).then(response => {
+      console.log(response);
+    });
   },
   components: {
     AppHeader,
