abfaf01816cd65c20ebe43a7ad828a6c043a8fc2
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-20 06:15:03 +0100

    feat: implemented client-side pagination logic in ProjectsList with full API data

--- src/components/ProjectsComponents/ProjectsList.vue
+++ src/components/ProjectsComponents/ProjectsList.vue
@@ -8,30 +8,58 @@ export default {
         return {
             apiUri: "http://127.0.0.1:8000/api/projects",
             projects: [],
+            currentPage: 1,
+            lastPage: 1
 
         }
     },
     methods: {
-        getProjects() {
-            axios.get(this.apiUri)
+        getProjects(pageNumber) {
+            axios.get(this.apiUri, {
+                params: {
+                    page: pageNumber,
+                }
+            })
                 .then((response) => {
                     console.log(response.data.results);
-                    this.projects = response.data.results;
+                    console.log(pageNumber);
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.current_page;
+                    this.lastPage = response.data.last_page;
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
+        },
+        changePage(newPage) {
+            this.getProjects(newPage);
 
 
         }
     },
     mounted() {
-        this.getProjects();
+        this.getProjects(1);
     },
 }
 </script>
 
 <template>
+
+    <section>
+
+        <h1>My Projects</h1>
+
+        <div v-for="project in projects" :key="project.id">
+            <h2>{{ project.project_title }}</h2>
+
+            <p>{{ project.summary }}</p>
+
+            <span>{{ project.type.project_type }}</span>
+
+        </div>
+
+
+    </section>
 </template>
 
 <style scoped></style>
