86bb86ad708b979d577e272ca46ee9b01edbe425
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 04:17:35 +0100

    feat: imported Axios in ProjectsList and implemented AJAX call to fetch projects from API

--- src/components/ProjectsComponents/ProjectsList.vue
+++ src/components/ProjectsComponents/ProjectsList.vue
@@ -1,28 +1,34 @@
 <script>
 // Imported Axios
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'ProjectsList',
+    name: "ProjectsList",
+    data() {
+        return {
+            apiUri: "http://127.0.0.1:8000/api/projects",
+            projects: [],
 
-},
-data() {
-    return {
-        apiUri: 'http://127.0.0.1:8000/api/projects',
-    }, methods: {
-        getProjects(){
+        }
+    },
+    methods: {
+        getProjects() {
             axios.get(this.apiUri)
                 .then((response) => {
                     console.log(response.data.results);
-                    this.ProjectsList = response.data.results;
+                    this.projects = response.data.results;
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
-        }
-    }
-}
 
+
+        }
+    },
+    mounted() {
+        this.getProjects();
+    },
+}
 </script>
 
 <template>
