715030d03a1c7196768bb1e8905736fa0f884477
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 17:56:54 +0100

    feat: added AppProjects component in pages folder

---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,37 @@
+<script>
+import ProjectsList from '../components/ProjectsComponents/ProjectsList.vue';
+export default {
+    name: "AppProjects",
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    created() {
+    },
+    components: {
+        ProjectsList,
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="h2 py-3">
+                        My Projects:
+                    </h2>
+                </div>
+                <div class="col-12">
+
+                    <ProjectsList />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
