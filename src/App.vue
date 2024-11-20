4fa3fe576eb43b1eccb9a87f7f449b636f020b87
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 03:27:34 +0100

    feat: removed API URI and axios call from App.vue component

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      apiUri: 'http://127.0.0.1:8000/api/projects',
     }
-  },
-  mounted() {
-    axios.get(this.apiUri).then(response => {
-      console.log(response);
-    });
   },
   components: {
     AppHeader,
