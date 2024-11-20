96bc7bc4e4af274fe5b9214fa82479923944c96e
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 07:04:22 +0100

    feat: added pageLinks prop, changePage emit, and navigation methods in AppPagination component

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -6,14 +6,20 @@ export default {
             type: Number,
             required: true,
         },
+
         lastPage: {
             type: Number,
+            required: true,
+        },
+
+        pageLinks: {
+            type: Array,
             required: true,
         },
         data() {
             return {
 
-            }
+            }, emits: ["changePage"],
         } computed: {
             FirstPage() {
                 return this.currentPage === 1;
@@ -22,10 +28,39 @@ export default {
                 return this.currentPage === this.lastPage;
             },
         },
-    }
+        methods: {
+            goToPreviousPage() {
+                if (!this.isPreviousDisabled) {
+                    this.$emit("changePage", this.currentPage - 1);
+                }
+            },
+            goToNextPage() {
+                if (!this.isNextDisabled) {
+                    this.$emit("changePage", this.currentPage + 1);
+                }
+            },
+        },
+    };
 </script>
 
 <template>
+    <div>
+        <nav>
+            <ul class="pagination">
+                <li class="page-item disabled">
+                    <a class="page-link">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <style scoped></style>
