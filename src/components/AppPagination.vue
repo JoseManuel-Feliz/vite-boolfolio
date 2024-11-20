297179f4ad8e9474ae8af8fb89ba5c0e6126bbfd
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-20 17:23:35 +0100

    feat: updated AppPagination to emit events for page navigation

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name: 'AppPagination',
+    name: "AppPagination",
     props: {
         currentPage: {
             type: Number,
@@ -12,57 +12,42 @@ export default {
             required: true,
         },
 
-        pageLinks: {
-            type: Array,
-            required: true,
-        },
-        data() {
-            return {
+    },
+    emits: ["previousPage", "nextPage", "changePage"],
+    data() {
+        return {
+        }
+    },
+    computed: {
 
-            }, emits: ["changePage"],
-        } computed: {
-            FirstPage() {
-                return this.currentPage === 1;
-            },
-            LastPage() {
-                return this.currentPage === this.lastPage;
-            },
-        },
-        methods: {
-            goToPreviousPage() {
-                if (!this.isPreviousDisabled) {
-                    this.$emit("changePage", this.currentPage - 1);
-                }
-            },
-            goToNextPage() {
-                if (!this.isNextDisabled) {
-                    this.$emit("changePage", this.currentPage + 1);
-                }
-            },
-        },
-    };
+    },
+    methods: {
+
+    },
+};
 </script>
 
 <template>
-    <div>
-        <nav>
-            <ul class="pagination">
-                <li class="page-item disabled">
-                    <a class="page-link">Previous</a>
-                </li>
-
-                <li v-for="(page, index) in pageLinks" :key="index" :class="['page-item', { active: page.active }]">
-                    <a class="page-link" href="#">
-                        {{ page.label }}
-                    </a>
-                </li>
-
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <nav>
+        <ul class="pagination">
+            <li :class="['page-item', { disabled: currentPage === 1 }]">
+                <button class="page-link" @click="$emit('previousPage')" :disabled="currentPage === 1">
+                    Previous
+                </button>
+            </li>
+            <li v-for="page in lastPage" :key="page" :class="['page-item', { active: page === currentPage }]">
+                <button class="page-link" @click="$emit('changePage', page)">
+                    {{ page }}
+                </button>
+            </li>
+            <li :class="['page-item', { disabled: currentPage === lastPage }]">
+                <button class="page-link" @click="$emit('nextPage')" :disabled="currentPage === lastPage">
+                    Next
+                </button>
+            </li>
+        </ul>
+    </nav>
 </template>
+
 
 <style scoped></style>
