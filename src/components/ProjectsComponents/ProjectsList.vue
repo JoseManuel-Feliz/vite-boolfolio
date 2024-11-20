479f0a066792dcd3f9463212d1b1202235e3e7c8
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-20 03:35:09 +0100

    feat: added axios import and configured API endpoint for projects in ProjectsList component

---
+++ src/components/ProjectsComponents/ProjectsList.vue
@@ -0,0 +1,31 @@
+<script>
+// Imported Axios
+import axios from 'axios';
+
+export default {
+    name: 'ProjectsList',
+
+},
+data() {
+    return {
+        apiUri: 'http://127.0.0.1:8000/api/projects',
+    }, methods: {
+        getProjects(){
+            axios.get(this.apiUri)
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.ProjectsList = response.data.results;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+}
+
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
